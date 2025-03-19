d7098aba1afa2e28b0d5cbc0301c898335e8a2d4
Isela-AH <isealvarezh@gmail.com>
2025-03-18 21:40:16 -0600

    Implementa una función para sortear los amigos
    Validar que haya amigos disponibles
    Generar un índice aleatorio
    Obtener el nombre sorteado
    Mostrar el resultado

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let amigos = [];
 
-function agregarAmigo()
+function agregarAmigo() {
     const inputAmigo = document.getElementById('amigo');
     const nombreAmigo = inputAmigo.value.trim();
 
@@ -10,22 +11,56 @@ function agregarAmigo()
         return; 
     }
 
-    if (amigos.includes/(nombreAmigo)){
+    if (amigos.includes(nombreAmigo)) {
         alert("Este amigo ya está en la lista.");
         return; 
     }
     
-    amigos.push/(nombreAmigo);
+    amigos.push(nombreAmigo);
     mostrarAmigos();
     inputAmigo.value = '';
+}
 
     function mostrarAmigos() {
-      const listaAmigos = document.getElmentById ('listaAmigos'); 
+      const listaAmigos = document.getElementById ('listaAmigos'); 
       listaAmigos.innerHTML = '';
-    }
+   
 
     amigos.forEach(amigo => {
         const li = document.createElement('li');
         li.textContent = amigo;
         listaAmigos.appendChild(li); 
     });
+ }
+
+    function sortearAmigo () {
+        if (amigos.length < 2) {
+            alert("Se necesitan al menos dos amigos para realizar el sorteo.");
+            return;
+        }
+
+        let amigosSorteados = [...amigos];
+        let resultados = {}; 
+
+        amigosSorteados.sort(() => Math.random() - 0.5);
+
+        for (let i = 0; i < amigos.length; i++) {
+            const amigo = amigos[i];
+            const amigoSecreto = amigosSorteados[i === amigos.length - 1 ? 0 : i + 1];
+            resultados[amigo] = amigoSecreto;
+        }
+        mostrarResultados(resultados);
+    }
+
+        function mostrarResultados(resultados) {
+            const listaResultado = document.getElementById('resultado');
+            listaResultado.innerHTML = '';
+
+            for (const [amigo, amigoSecreto] of Object.entries(resultados)) {
+                const li = document.createElement('li');
+                li.textContent = `${amigo} le tocó a ${amigoSecreto}`;
+                listaResultado.appendChild(li);
+
+        }
+    }
+            
