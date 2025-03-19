724ea227239c0ba2f72383963ab9cea274b7609c
Isela-AH <isealvarezh@gmail.com>
2025-03-18 20:33:54 -0600

    Agregando Array

---
+++ app.js
@@ -0,0 +1,7 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo()
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.value.trim();
+    
