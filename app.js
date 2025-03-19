aebf10651a54105b1158a458360f726d9699df2d
Isela-AH <isealvarezh@gmail.com>
2025-03-18 20:46:38 -0600

    implementar función para agregar amigos
    Capturar el valor del campo de entrada
    Validar la entrada
    Actualizar el array de amigos
    Limpiar el campo de entrada

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ let amigos = [];
 function agregarAmigo()
     const inputAmigo = document.getElementById('amigo');
     const nombreAmigo = inputAmigo.value.trim();
-    
+
+    if (!nombreAmigo) {
+        alert("Por favor, ingresa un nombre.");
+        return; 
+    }
+
+    amigos.push/(nombreAmigo);
+    mostrarAmigos();
+    inputAmigo.value = '';
